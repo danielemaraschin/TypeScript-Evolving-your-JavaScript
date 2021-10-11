de68cd2664bf23f19001d5a5463a263d1b8d379d
danielemaraschin <danikmaraschin@gmail.com>
2021-10-11 10:06:29 +0200

    tsc -w

--- app/app.ts
+++ app/app.ts
@@ -3,5 +3,3 @@ import { Negociacao } from "./models/negociacao.js";
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.data);
 console.log(negociacao.volume);
-
-alert("test watch mode")
