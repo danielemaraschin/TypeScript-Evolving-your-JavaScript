d668ed4a07086480fb3753e8295ebccfbc03896c
danielemaraschin <danikmaraschin@gmail.com>
2021-10-12 13:17:22 +0200

    new method

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -7,6 +7,10 @@ export class NegociacaoController{
         this.inputData = document.querySelector("#data"); //input data tem id-data no index.html
         this.inputQuantidade =document.querySelector("#quantidade");
         this.inputValor = document.querySelector("#valor")
-
     }
+        adiciona(){
+            console.log(this.inputData);
+            console.log(this.inputQuantidade);
+            console.log(this.inputValor);
+        }
 }
