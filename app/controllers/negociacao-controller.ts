04421510668a15f59ce59babdc82fab3fc70fc61
danielemaraschin <danikmaraschin@gmail.com>
2021-10-12 12:35:39 +0200

    constructor  controller

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -2,4 +2,11 @@ export class NegociacaoController{
     private inputData;
     private inputQuantidade;
     private inputValor;
+
+    constructor(){
+        this.inputData = document.querySelector("#data"); //input data tem id-data no index.html
+        this.inputQuantidade =document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor")
+
+    }
 }
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,2 +1,7 @@
 export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data"); //input data tem id-data no index.html
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
 }
