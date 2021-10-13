7b464ec1938e9203bd11aa0c66208fac56d5efe7
danielemaraschin <danikmaraschin@gmail.com>
2021-10-13 22:43:54 +0200

    new array

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,14 +2,26 @@ import { Negociacao } from "../models/negociacao.js";
 
 // esse modelo de negociacoes, vai encapsular a lista de negociacoes
 
-export class  Negociacoes {
+export class Negociacoes {
     private negociacoes: Array<Negociacao> = [] //so a instancia de negociacoes que pode alterar o array negociacoes
-//tipo ARRAY que vvai receber dentro Negociacao
+    //tipo ARRAY que vvai receber dentro Negociacao
 
-    adiciona(negociacao: Negociacao){ //o parametro negociacao vai ser do tipo : Negociacao
+    adiciona(negociacao: Negociacao) { //o parametro negociacao vai ser do tipo : Negociacao
         this.negociacoes.push(negociacao);//recebe essa negociacao e faz um push na lista de negociacoes
-    } 
+    }
+
+    lista(): Array<Negociacao> {
+        return this.negociacoes;
+    }
 }
 
-const negociacoes = new Negociacoes()
-negociacoes.adiciona(new Negociacao());
+// document.querySelector('form').addEventListener('submit', (event) => {
+    // var tr = document.createElement("tr");
+// 
+    // const negociacoes = new Negociacoes()
+    // negociacoes.lista().forEach(n => {
+        // var td = document.createElement("td");
+        // td.textContent = n.value
+    // }
+// )}
+        
