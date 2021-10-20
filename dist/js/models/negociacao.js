0d80a499d8b72ab4f131e7ec3f2814a5734039e0
danielemaraschin <danikmaraschin@gmail.com>
2021-10-20 15:18:29 +0200

    working with readonly

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,13 +1,13 @@
 export class Negociacao {
-    constructor(_data, _quantidade, _valor) {
+    constructor(_data, quantidade, valor) {
         this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get data() {
         return this._data;
     }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
