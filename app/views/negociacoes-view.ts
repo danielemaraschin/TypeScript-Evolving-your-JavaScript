60da150d5f3df76fa667a0811dd789afb5d65059
danielemaraschin <danikmaraschin@gmail.com>
2021-10-20 17:44:35 +0200

    Working with template string and HTML

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,5 +1,15 @@
 export class NegociacoesView{
-    template(): string{
-        
+    template(): string{ //retorna uma string HTML // primeiro retornsa a template string
+        return`
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            
+            
+            </thead>`
     }
 }
