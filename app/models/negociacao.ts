73335870a75828fd8a7a36585c1d4b442322123e
danielemaraschin <danikmaraschin@gmail.com>
2021-10-20 14:22:16 +0200

    simplifying the constructor class declaration

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,16 +1,20 @@
 export class Negociacao {
 
-    constructor(
-        private _data: Date,
-        private _quantidade: number,
-        private _valor: number
-    ) { }
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
+   
+    constructor(data: Date ,quantidade: number, valor: number){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
 
-    get data(): Date {
+    get data(): Date{
         return this._data;
     }
 
-    get quantidade(): number {
+    get quantidade(): number{
         return this._quantidade;
     }
 
@@ -18,7 +22,7 @@ export class Negociacao {
         return this._valor;
     }
 
-    get volume(): number {
+    get volume(): number{
         return this._quantidade * this._valor;
     }
 
