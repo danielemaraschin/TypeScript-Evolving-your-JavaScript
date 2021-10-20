c11a7fdda93495b444283331fa410a03245d1fe3
danielemaraschin <danikmaraschin@gmail.com>
2021-10-20 15:14:24 +0200

    working with readonly

---
+++ dist/js/exercise/atalhoConstructorOriginal.js
@@ -0,0 +1,7 @@
+export class Fatura {
+    constructor(criadaEm, quantidade, valor) {
+        this.criadaEm = criadaEm;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+}
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -3,7 +3,7 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = []; //so a instancia de negociacoes que pode alterar o array negociacoes
     }
-    //tipo ARRAY que vvai receber dentro Negociacao
+    //tipo ARRAY que vai receber dentro Negociacao
     adiciona(negociacao) {
         this.negociacoes.push(negociacao); //recebe essa negociacao e faz um push na lista de negociacoes
     }
