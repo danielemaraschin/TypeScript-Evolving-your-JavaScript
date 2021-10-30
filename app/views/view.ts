7800c637cb833f29abe0590f4be95d15bb933467
danielemaraschin <danikmaraschin@gmail.com>
2021-10-30 17:48:59 +0200

    working with inheritance

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,3 +1,13 @@
 export  class View{
-    
+
+    public elemento: HTMLElement
+    constructor(seletor: string){
+        this.elemento = document.querySelector(seletor)
+    }
+    template(model: string){
+        throw new Error("Template has to be implemented.")
+    }
+    update(model: string): void{
+        this.elemento.innerHTML = this.template(model)
+    }
 }
