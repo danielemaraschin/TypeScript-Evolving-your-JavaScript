e41cd8f8d0115edd5145cf6fc83bb67542635c76
danielemaraschin <danikmaraschin@gmail.com>
2021-10-31 17:08:53 +0100

    exercise about generics: 2 types, 1 class

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -28,11 +28,15 @@ export class NegociacoesView extends View<Negociacoes>{
             </tbody>
         <table>`
     }
+}
+
+
+//o código update foi apagado pq ele está sendo herdado da classe pai, mas vou deixar o codigo aqui caso precise consultar dps
     // update(model: Negociacoes): void { //por esse metodo pedimos para a View fazer o seu proprio update --
     //     this.elemento.innerHTML = this.template(model); //transforma codigo HTML em elemento do DOM
     //         //update e template recebem o mesmo parametro pq no final o template que vai passar os dados para elem do dom no final
     //}
-}
+//}
 
 // fazemos lista().map  para q cada item da lista retorne a tr com suas td preenchidas com os dados em js ${}
 //mas para essa string nao ser um array, precisamos add o metodo .join() , sem nenhuma separação entre os itens do array (que deixarão de ser array)
