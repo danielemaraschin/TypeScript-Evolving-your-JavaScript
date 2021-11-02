38dfedcfcae269936d66c25ebe9e418ff91a00da
danielemaraschin <danikmaraschin@gmail.com>
2021-11-02 17:14:18 +0100

    Learning about abstract method

--- app/views/view.ts
+++ app/views/view.ts
@@ -4,8 +4,9 @@ export abstract class View<T>{
     constructor(seletor: string){
         this.elemento = document.querySelector(seletor)
     }
-    template(model: T) : string{
-        throw new Error("Template has to be implemented.");
+    abstract template(model: T) : string{
+        //throw new Error("Template has to be implemented.");
+        //using abstract method it's not need throw new error
     }
     update(model: T): void{
         const template = this.template(model)
