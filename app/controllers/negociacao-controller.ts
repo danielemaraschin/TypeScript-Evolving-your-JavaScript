f400da484cd48e1401bf6bfc3e40ff5d66974ae4
danielemaraschin <danikmaraschin@gmail.com>
2021-11-10 07:44:53 +0100

    accepting only weekdays readonly

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -10,7 +10,7 @@ export class NegociacaoController{
     private negociacoes: Negociacoes = new Negociacoes; //pode remover a tipagem pq já está inicializando o atributo
     private negociacaoesView  = new NegociacoesView("#negociacoesView")
     private mensagemView = new MensagemView("#mensagemView")
-    private SABADO = 6; //var em letra maiucula no ts
+    private readonly SABADO = 6; //var em letra maiucula no ts
     private DOMINGO = 0;
 
 
