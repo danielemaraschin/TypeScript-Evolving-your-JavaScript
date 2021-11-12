ec9b2ca518f71be14ef63bda0af55693cfd7d390
danielemaraschin <danikmaraschin@gmail.com>
2021-11-12 17:54:14 +0100

    Isolating input conversion

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -14,7 +14,7 @@ export class Negociacao {
     //modifica-la mesmo que seja readonly entao nao adianta usar o readonly
 
 
-    public criaDe(dataString: string, quantidadeString: string, valorString: string) {
+    public criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
         const date = new Date(dataString.replace(/-/g, ", "));
         const quantidade = parseInt(quantidadeString)
         const valor = parseFloat(valorString);
