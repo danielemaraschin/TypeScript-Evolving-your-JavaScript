45260504993e261284b4dc48d973faa67f247462
danielemaraschin <danikmaraschin@gmail.com>
2021-11-16 11:59:02 +0100

    protecting our template

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,13 +1,16 @@
 export abstract class View<T>{
 
     protected elemento: HTMLElement
-    constructor(seletor: string){
+
+    constructor(seletor: string, escape: boolean){
         this.elemento = document.querySelector(seletor)
     }
+
     protected abstract template(model: T) : string
         //throw new Error("Template has to be implemented.");
         //using abstract method it's not need throw new error
     
+
     public update(model: T): void{
         const template = this.template(model)
         this.elemento.innerHTML = template;
